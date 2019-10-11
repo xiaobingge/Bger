37ae14c3d8098e965a21ddccca4c331582d981bd
bingge <bingge@juanpi.com>
2019-10-11 16:05:42 +0800

    优化

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('user/login', function (Request $request) {
+
+    echo $request->input('code');
+});
 //后台管理系统路由
 Route::any('admin/loginCenter', 'AdminController@login');
 Route::group(['middleware' => ['api', 'multiauth:admin',]], function () {
