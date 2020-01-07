135ba04762800db4f315daa6d2482780ab356b47
bingge <bingge@juanpi.com>
2020-01-07 17:54:46 +0800

    关注回复

--- app/Services/Wechat/EventMessageHandle.php
+++ app/Services/Wechat/EventMessageHandle.php
@@ -25,42 +25,43 @@ class EventMessageHandler implements  EventHandlerInterface
         $this->message=$payload;
         if(in_array($this->message['Event'],['subscribe','SCAN'])){
             $rule_id = 0;
-            if(!empty($this->message['EventKey'])){ //二维码扫描关注
-                $arr = explode('_',$this->message['EventKey']);
+            if(!empty($this->message['EventKey'])) { //二维码扫描关注
+                $arr = explode('_', $this->message['EventKey']);
                 $keyword = $this->message['Event'] == 'subscribe' ? $arr[1] : $this->message['EventKey'];
-                $rule = Rules::where(['keyword'=>$keyword])->first();
-                if(!empty($rule->id))
+                $rule = Rules::where(['keyword' => $keyword])->first();
+                if (!empty($rule->id))
                     $rule_id = $rule->id;
-                $reply = Reply::where(['rule_id'=>$rule_id])->orderBy('id','asc')->get();
-                if(!$reply->isEmpty()){
-                    $items = [];
-                    foreach($reply as $key=>$value){
-                        if($value->type == 1){
-                            $items[] =  new Text($value->content);
-                        }elseif($value->type == 2){
-                            $items[] =  new Image($value->media_id);
-                        }elseif($value->type == 3){
-                            $items[]  = new Media($value->media_id, 'mpnews' );
-                        }
-                    }
-                    if(!empty($items)){
-                        $config = config('wechat.official_account.default');
-                        $app = Factory::officialAccount($config);
-                        if($rule->reply_mode == 1){ //全部回复
-                            foreach($items as $msg ){
-                                $app->customer_service->message($msg)->to($this->message['FromUserName'])->send();
-                                sleep(1);
-                            }
-                        }else{   //随机回复一条
-                            $mod = array_rand($items);
-                            $app->customer_service->message($items[$mod])->to($this->message['FromUserName'])->send();
-                            //素材消息不支持被动回复 走客服接口
-                            //return $items[$mod];
-                        }
-
+            }
+            $reply = Reply::where(['rule_id'=>$rule_id])->orderBy('id','asc')->get();
+            if(!$reply->isEmpty()){
+                $items = [];
+                foreach($reply as $key=>$value){
+                    if($value->type == 1){
+                        $items[] =  new Text($value->content);
+                    }elseif($value->type == 2){
+                        $items[] =  new Image($value->media_id);
+                    }elseif($value->type == 3){
+                        $items[]  = new Media($value->media_id, 'mpnews' );
                     }
                 }
+                if(!empty($items)){
+                    $config = config('wechat.official_account.default');
+                    $app = Factory::officialAccount($config);
+                    if($rule->reply_mode == 1){ //全部回复
+                        foreach($items as $msg ){
+                            $app->customer_service->message($msg)->to($this->message['FromUserName'])->send();
+                            sleep(1);
+                        }
+                    }else{   //随机回复一条
+                        $mod = array_rand($items);
+                        $app->customer_service->message($items[$mod])->to($this->message['FromUserName'])->send();
+                        //素材消息不支持被动回复 走客服接口
+                        //return $items[$mod];
+                    }
+
+                }
             }
+
 
         }
 
