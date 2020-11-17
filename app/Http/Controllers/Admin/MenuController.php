1b15cabf2fbb2651e29d44ea7f09d1100bb30dae
bingge <bingge@juanpi.com>
2020-11-17 14:22:56 +0800

    文档

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Menus;
 use Illuminate\Http\Request;
+use OpenApi\Annotations\Flow;
+use OpenApi\Annotations\OpenApi;
 use Spatie\Permission\Models\Permission;
 
 /**
